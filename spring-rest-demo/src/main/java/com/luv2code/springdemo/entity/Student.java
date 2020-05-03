6c1cabb8c96a739748d81cee1bf991c20a65f8d8
23Vatsal <vatsalbhatnagar007@gmail.com>
2020-05-03 20:29:13 +0530

    Retrieving a single student using an Id.

--- spring-rest-demo/src/main/java/com/luv2code/springdemo/entity/Student.java
+++ spring-rest-demo/src/main/java/com/luv2code/springdemo/entity/Student.java
@@ -2,6 +2,7 @@ package com.luv2code.springdemo.entity;
 
 public class Student {
 	
+	
 	private String firstName;
 	private String lastName;
 	
--- spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
+++ spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
@@ -3,7 +3,10 @@ package com.luv2code.springdemo.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,16 +16,35 @@ import com.luv2code.springdemo.entity.Student;
 @RequestMapping("/api")
 public class StudentRestController {
 	
-	@GetMapping("/students")
-	public List<Student> getStudents(){
-		List<Student> theStu=new ArrayList<>();
+	//adding the students here because if we add it inside the method then
+	//every time the page is loaded the data is added.
+	//use @PostConstruct to load student data.(ONLY ONCE)
+	
+	private List<Student> theStu;
+	
+	@PostConstruct
+	public void addStu() {
+		theStu=new ArrayList<>();
 		
 		theStu.add(new Student("Vatsal","Bhatnagar"));
 		theStu.add(new Student("Vasu","Bhatnagar"));
 		theStu.add(new Student("Abc","Xyz"));
 		
+	}
+	//endpoint for retrieving all students
+	
+	@GetMapping("/students")
+	public List<Student> getStudents(){
+				
 		return theStu;
 		
+	}
+	
+	//endpoint for retrieving a single student using studentId
+	@GetMapping("/students/{studentId}")
+	public Student getStudent(@PathVariable int studentId) {
+		
+		return theStu.get(studentId);
 	}
 
 }
