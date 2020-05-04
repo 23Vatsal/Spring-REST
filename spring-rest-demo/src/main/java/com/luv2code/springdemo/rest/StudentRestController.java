db651e93d03308d5d4f1648646c6593a42e99a71
23Vatsal <vatsalbhatnagar007@gmail.com>
2020-05-04 19:13:00 +0530

    Added functionality: Exception Handling

--- spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
+++ spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
@@ -5,6 +5,9 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -44,7 +47,40 @@ public class StudentRestController {
 	@GetMapping("/students/{studentId}")
 	public Student getStudent(@PathVariable int studentId) {
 		
+		//check if studentId is in valid range.
+		//if not throw the exception we created.
+		if((studentId>theStu.size()) || (studentId<0)) {
+			throw new StudentNotFoundException("No student found id => "+studentId);
+		}
+		
 		return theStu.get(studentId);
+	}
+	
+	//adding exception handler using @ExceptionHandler
+	@ExceptionHandler
+	public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc){
+		
+		StudentErrorResponse error =new StudentErrorResponse();
+		
+		error.setStatus(HttpStatus.NOT_FOUND.value());
+		error.setMessage(exc.getMessage());
+		error.setTimeStamp(System.currentTimeMillis());
+		
+		return new ResponseEntity<>(error,HttpStatus.NOT_FOUND);
+	}
+	
+	//adding exception handler to catch any type of error(Generic exception handler)
+	@ExceptionHandler
+	public ResponseEntity<StudentErrorResponse> handleException(Exception exc){
+		
+		StudentErrorResponse error =new StudentErrorResponse();
+		
+		error.setStatus(HttpStatus.BAD_REQUEST.value());
+		error.setMessage(exc.getMessage());
+		error.setTimeStamp(System.currentTimeMillis());
+		
+		
+		return new ResponseEntity<>(error,HttpStatus.BAD_REQUEST);
 	}
 
 }
